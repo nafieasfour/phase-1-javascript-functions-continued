8f4236049e0668ace7c0c5988b029cbb23572cba
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-04-29 21:29:30 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(action = "roller-skate") {
+    return (`This Saturday, I want to ${action}!`);
+  }
+  
+saturdayFun();
+saturdayFun("bathe my dog")
+
+function mondayWork(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+mondayWork()
+mondayWork("work from home")
+
+
+function wrapAdjective(string="*") {
+    return function (word = "special") {
+      return `You are ${string}${word}${string}!`;
+    };
+  }
+  
+wrapAdjective()(word="hard worker");
+
